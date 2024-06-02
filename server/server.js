084fe081c5266997be9c306b750a6cae99be6710
Alec-Nesat Colak <acolak9@ufl.edu>
2024-06-01 21:35:49 -0400

    cors update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://https://alec-portfolio-phi.vercel.app"],
+    origin: true,
     methods: ["POST", "GET"],
     credentials: true,
   })
