9754af9dd39502bbf9a0ff4994a51a947dba3212
Alec-Nesat Colak <acolak9@ufl.edu>
2024-06-01 20:15:22 -0400

    replaced backend url

--- client/src/pages/Admin/AdminProjects.js
+++ client/src/pages/Admin/AdminProjects.js
@@ -21,12 +21,12 @@ function Projects() {
       dispatch(ShowLoading());
       let response;
       if (selectedItemForEdit) {
-        response = await axios.post("/api/portfolio/update-project", {
+        response = await axios.post("https://alec-portfolio-9j5d.onrender.com/api/portfolio/update-project", {
           ...values,
           _id: selectedItemForEdit._id,
         });
       } else {
-        response = await axios.post("/api/portfolio/add-project", values);
+        response = await axios.post("https://alec-portfolio-9j5d.onrender.com/api/portfolio/add-project", values);
       }
       dispatch(HideLoading());
       if (response.data.success) {
@@ -48,7 +48,7 @@ function Projects() {
   const onDelete = async (item) => {
     try {
       dispatch(ShowLoading());
-      const response = await axios.post("/api/portfolio/delete-project", {
+      const response = await axios.post("https://alec-portfolio-9j5d.onrender.com/api/portfolio/delete-project", {
         _id: item._id,
       });
       dispatch(HideLoading());
--- server/server.js
+++ server/server.js
@@ -4,15 +4,17 @@ const cors = require("cors");
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 
-const portfolioRoute = require("./api");
+const portfolioRoute = require("./routes/portfolioRoute");
 
 app.use(express.json());
 
-app.use(cors({
-  origin: ["https://alec-portfolio-two.vercel.app/"],
-  methods: ["POST", "GET"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://alec-portfolio-two.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json("Welcome to Alec's Portfolio");
